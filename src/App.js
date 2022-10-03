c5f1219e5ad6dddeb37b66a27c48c5316b2e5acb
skdding3 <skdding3@gmail.com>
2022-10-03 20:12:24 +0900

    react 연마하기 10/3
    - eslint 8.22 다운그레이드
    - 리액트 연산자

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const name = '리액'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      // ? 연산자 연습 (like if)
+    <div>
+        {name === '리액트' ? <h1>리액트 입니다.</h1> : <h1>리액트 아닙니다.</h1>}
     </div>
   );
 }
