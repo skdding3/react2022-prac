659168b9ec00247c8dd1ce4ea58016ee35bb1ae6
skdding3 <skdding3@gmail.com>
2022-10-10 17:32:16 +0900

    react 연마하기 10/10
    - #1 component 생성 및 props 렌더링

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import MyComponent from "./MyComponent";
 
 function App() {
-    // name 프로퍼티에 넣어줄 값을 넣어준다.
+    // name 프로퍼티에 넣어줄 값을 넣어준다!
     return <MyComponent name={"Sunki"}/>
 }
 
--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MyComponent = props => {
     // name 프로퍼티를 지정해준다.
-    return <div>안녕하세요, 제 이름은 {props.name} 입니다.</div>
+    return <div>안녕하세요, 내 이름은 {props.name} 입니다.</div>
 }
 
 export default MyComponent;
