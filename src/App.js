b35989f7b8fd57e039bf5be8fa6a685f270f8051
skdding3 <skdding3@gmail.com>
2022-10-10 19:23:42 +0900

    react 연마하기 10/10
    - #1 component 생성 및 props 렌더링
    - #2 클래스형 컴포넌트에서 props 사용하기
    - #3 클래스형 컴포넌트의 state
    - #4 useState 사용하기
    - #5 이벤트 핸들링

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import Say from "./component/Say";
+import EventPractice from "./component/EventPractice";
+// import Say from "./component/Say";
 // import MyComponent from "./MyComponent";
 // import Counter from "./component/Counter";
 
 function App() {
-    return <Say />
+    return <EventPractice />
 }
 
 export default App;
