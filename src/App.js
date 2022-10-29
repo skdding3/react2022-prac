ba9de40482e7c9c2d26c9bcb2c846f1883c1abf1
skdding3 <skdding3@gmail.com>
2022-10-29 15:17:38 +0900

    ValidationSample for React (Create Sample)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 // import EventPractice from "./component/EventPractice";
-import Counter from "./component/Counter";
+// import Counter from "./component/Counter";
+import ValidationSample from "./component/ValidationSample";
 // import Say from "./component/Say";
 // import MyComponent from "./MyComponent";
 
 function App() {
-    return <Counter />
+    return <ValidationSample />
 }
 
 export default App;
