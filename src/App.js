cbc939631ee29357e892b0acb83ed6a2e28f4c07
skdding3 <skdding3@gmail.com>
2022-10-29 15:54:33 +0900

    ref by ScrollBox

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
+import "./App.css";
+import ScrollBox from "./component/ScrollBox";
 // import EventPractice from "./component/EventPractice";
 // import Counter from "./component/Counter";
-import ValidationSample from "./component/ValidationSample";
+// import ValidationSample from "./component/ValidationSample";
 // import RefSample from "./component/RefSample";
 // import Say from "./component/Say";
 // import MyComponent from "./MyComponent";
 
 function App() {
-    return <ValidationSample />
+  return <ScrollBox />;
 }
 
 export default App;
