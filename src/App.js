b575bfda61db32bfe8867461cc7fae1ae8b324f7
skdding3 <skdding3@gmail.com>
2022-11-14 23:08:40 +0900

    IterationCode (map 형태 작성)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
-import ScrollBox from "./component/ScrollBox";
+// import ScrollBox from "./component/ScrollBox";
 // import EventPractice from "./component/EventPractice";
 // import Counter from "./component/Counter";
 // import ValidationSample from "./component/ValidationSample";
 // import RefSample from "./component/RefSample";
 // import Say from "./component/Say";
 // import MyComponent from "./MyComponent";
+import IterationSample from "./component/iterationSample";
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        {/*컴포넌트 ref*/}
-        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          최하단으로 이동
-        </button>
-      </div>
-    );
+    return <IterationSample />;
   }
 }
 
--- src/component/iterationSample.jsx
+++ src/component/iterationSample.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 
+// const IterationSample = () => {
+//   return (
+//     <ul>
+//       <li>눈사람</li>
+//       <li>얼음</li>
+//       <li>눈</li>
+//       <li>바다</li>
+//     </ul>
+//   );
+// };
+
 const IterationSample = () => {
-  return (
-    <ul>
-      <li>눈사람</li>
-      <li>얼음</li>
-      <li>눈</li>
-      <li>바다</li>
-    </ul>
-  );
+  const names = ["눈사람", "얼음", "눈", "바다"];
+  const nameList = names.map((name) => <li>{name}</li>);
+  return <ul>{nameList}</ul>;
 };
 
 export default IterationSample;
