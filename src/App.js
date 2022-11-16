041e49dc2552dd1796caaaca4e05aea1d30bd465
skdding3 <skdding3@gmail.com>
2022-11-17 08:37:32 +0900

    Component LifeCycle

--- src/App.js
+++ src/App.js
@@ -7,11 +7,31 @@ import "./App.css";
 // import RefSample from "./component/RefSample";
 // import Say from "./component/Say";
 // import MyComponent from "./MyComponent";
-import IterationSample from "./component/iterationSample";
+// import IterationSample from "./component/iterationSample";
+import LifeCycleSample from "./component/LifeCycleSample";
+
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: "#000000",
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
   render() {
-    return <IterationSample />;
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color} />
+      </div>
+    );
   }
 }
 
