a1d43220a34f34ab624befc99eabbbf17203b41f
skdding3 <skdding3@gmail.com>
2022-11-24 17:02:30 +0900

    rtk query tutorial

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React, { Component } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./routes/Home";
-import Detail from "./routes/Detail";
+import React from "react";
+import { useGetPokemonByNameQuery } from "./services/pokemon";
+// import Home from "./routes/Home";
+// import Detail from "./routes/Detail";
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" exact element={<Home />}></Route>
-          <Route path="/:id" element={<Detail />}></Route>
-        </Routes>
-      </BrowserRouter>
-    );
-  }
+export default function App() {
+  const { data, error, isLoading } = useGetPokemonByNameQuery("bulbasaur");
+
+  return (
+    <div className="App">
+      {error ? (
+        <>Oh no, there was an error</>
+      ) : isLoading ? (
+        <>Loading...</>
+      ) : data ? (
+        <>
+          <h3>{data.species.name}</h3>
+          <img src={data.sprites.front_shiny} alt={data.species.name} />
+        </>
+      ) : null}
+    </div>
+  );
 }
-
-export default App;
--- src/store/store.js
+++ src/store/store.js
@@ -1,20 +1,39 @@
-import { v4 as uuidv4 } from "uuid";
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
+import { pokemonApi } from "../services/pokemon";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
-const toDos = createSlice({
-  name: "toDosReducer",
-  initialState: [],
-  reducers: {
-    // Action creator
-    add: (state, action) => {
-      state.push({ text: action.payload, id: uuidv4() });
-    },
-    // Action creator
-    remove: (state, action) =>
-      state.filter((toDo) => toDo.id !== action.payload),
+/**
+ * Todo List
+ * @type {Slice<*[], {add: reducers.add, remove: (function(*, *): *)}, string>}
+ */
+// const toDos = createSlice({
+//   name: "toDosReducer",
+//   initialState: [],
+//   reducers: {
+//     // Action creator
+//     add: (state, action) => {
+//       state.push({ text: action.payload, id: uuidv4() });
+//     },
+//     // Action creator
+//     remove: (state, action) =>
+//       state.filter((toDo) => toDo.id !== action.payload),
+//   },
+// });
+//
+// export const { add, remove } = toDos;
+//
+// export default configureStore({ reducer: toDos.reducer });
+
+export const store = configureStore({
+  reducer: {
+    // 특정 top level slice에서 생성된 리듀서를 추가
+    [pokemonApi.reducerPath]: pokemonApi.reducer,
   },
+  // 캐싱, 요청취소. 풀링 등 유용한 rtk-query의 기능들을 위한 api 미들웨어 추가
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(pokemonApi.middleware),
 });
 
-export const { add, remove } = toDos;
+setupListeners(store.dispatch);
 
-export default configureStore({ reducer: toDos.reducer });
+export default store;
