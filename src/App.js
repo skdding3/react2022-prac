5018485ded72ee43c9bd2e2b1b4dc6e916a7a201
skdding3 <skdding3@gmail.com>
2022-11-28 18:36:52 +0900

    rtk query advanced

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { useGetPokemonByNameQuery } from "./services/pokemon";
 // import Home from "./routes/Home";
 // import Detail from "./routes/Detail";
 
 export default function App() {
   const { data, error, isLoading } = useGetPokemonByNameQuery("bulbasaur");
+  const { pollingInterval, setPollingInterval } = useState(0);
 
   return (
     <div className="App">
+      <select
+        onChange={(change) => setPollingInterval(Number(change.target.value))}
+      >
+        <option value={0}>OFF</option>
+      </select>
       {error ? (
         <>Oh no, there was an error</>
       ) : isLoading ? (
