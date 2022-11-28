db87b45070295db9fde776ed46fdd02308da94e8
skdding3 <skdding3@gmail.com>
2022-11-29 08:38:13 +0900

    useEffect Prac

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
-import React, { useState } from "react";
-import { useGetPokemonByNameQuery } from "./services/pokemon";
+import React from "react";
+import Info from "./component/Info";
+
 // import Home from "./routes/Home";
 // import Detail from "./routes/Detail";
+const App = () => {
+  return <Info />;
+};
 
-export default function App() {
-  const { data, error, isLoading } = useGetPokemonByNameQuery("bulbasaur");
-  const { pollingInterval, setPollingInterval } = useState(0);
-
-  return (
-    <div className="App">
-      <select
-        onChange={(change) => setPollingInterval(Number(change.target.value))}
-      >
-        <option value={0}>OFF</option>
-      </select>
-      {error ? (
-        <>Oh no, there was an error</>
-      ) : isLoading ? (
-        <>Loading...</>
-      ) : data ? (
-        <>
-          <h3>{data.species.name}</h3>
-          <img src={data.sprites.front_shiny} alt={data.species.name} />
-        </>
-      ) : null}
-    </div>
-  );
-}
+export default App;
--- src/component/Info.jsx
+++ src/component/Info.jsx
@@ -1,24 +1,40 @@
-import React from "react";
-import Hooks from "./Hooks";
+import React, { useEffect, useState } from "react";
 
 const Info = () => {
-  const [state, onChange] = Hooks({
-    name: "",
-    nickname: "",
+  const [name, setName] = useState("");
+  const [nickname, setNickname] = useState("");
+
+  useEffect(() => {
+    console.log("렌더링이 완료되었습니다.");
+    console.log({
+      name,
+      nickname,
+    });
   });
-  const { name, nickname } = state;
+
+  const onChangeName = (e) => {
+    setName(e.target.value);
+  };
+
+  const onChangeNickname = (e) => {
+    setNickname(e.target.value);
+  };
+
   return (
     <div>
       <div>
-        <input name="name" value={name} onChange={onChange} />
-        <input name="nickname" value={nickname} onChange={onChange} />
+        <input value={name} onChange={onChangeName} />
+        <input value={nickname} onChange={onChangeNickname} />
       </div>
       <div>
-        <b>이름:</b> {name}
+        <div>
+          <b>이름:</b> {name}
+        </div>
       </div>
       <div>
-        <b>닉네임:</b>
-        {nickname}
+        <div>
+          <b>닉네임:</b> {nickname}
+        </div>
       </div>
     </div>
   );
