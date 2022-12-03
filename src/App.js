bb4069b699997fd889753506dc16cb98ad574ae2
skdding3 <skdding3@gmail.com>
2022-12-04 00:43:59 +0900

    useEffect Prac

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from "react";
-import Info from "./component/Info";
+import React, { useState, useEffect } from "react";
 
-// import Home from "./routes/Home";
-// import Detail from "./routes/Detail";
-const App = () => {
-  return <Info />;
-};
+function App() {
+  // State
+  const [count, setCount] = useState(1);
+  const [name, setName] = useState("");
+
+  const handleCountUpdate = () => {
+    setCount(count + 1);
+  };
+
+  const handleInputChange = (e) => {
+    setName(e.target.value);
+  };
+  // Effect => 렌더링 될때 마다 실행됨 => 마운트 + [ item ] 이 변경될떄만 적용
+  useEffect(() => {
+    console.log("count 변화");
+  }, [count]);
+
+  return (
+    <div>
+      <button onClick={handleCountUpdate}>Update</button>
+      <span>count: {count}</span>
+      <input type="text" value={name} onChange={handleInputChange} />
+      <span>name: {name}</span>
+    </div>
+  );
+}
 
 export default App;
